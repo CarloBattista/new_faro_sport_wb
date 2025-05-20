44d8639d0faa2f77a57025fb76d72840da04e3fa
CarloBattista <carlitobatti@gmail.com>
2025-05-20 22:39:23 +0200

    added google maps section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
     <navbar />
     <hero :home="true" />
+    <mapSection />
 </template>
 
 <script>
 import navbar from "../components/global/navbar.vue"
 import hero from "../components/global/hero.vue"
+import mapSection from "../components/section/map-section.vue"
 
 export default {
     name: "Home",
     components: {
         navbar,
-        hero
+        hero,
+        mapSection
     }
 }
 </script>
