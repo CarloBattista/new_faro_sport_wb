f0c514c57f307b2550bf90e08e7859118be8a6ef
CarloBattista <carlitobatti@gmail.com>
2025-05-24 18:01:47 +0200

    fix card structure

--- src/components/card/card-structure.vue
+++ src/components/card/card-structure.vue
@@ -2,14 +2,14 @@
     <div class="relative w-full max-w-[380px] bg-black" :class="{ 'pointer-events-none opacity-50': data?.coming_soon }" style="aspect-ratio: 380 / 580;">
         <div class="relative z-10 w-full h-full">
             <div class="absolute linear-vignette z-[3] top-0 left-0 w-full h-full opacity-70"></div>
-            <div class="absolute block-vignette z-[2] top-0 left-0 w-full h-full bg-black/20"></div>
+            <div class="absolute block-vignette z-[2] top-0 left-0 w-full h-full bg-black/60"></div>
             <img :src="`/_resources/images/${data?.hero_image}`" :alt="data?.title" class="w-full h-full object-cover" loading="lazy" fetchpriority="high">
         </div>
         <div class="absolute z-20 top-0 left-0 w-full h-full px-6">
             <div class="w-full h-full flex flex-col gap-11 justify-center">
                 <div class="flex flex-col gap-1">
-                    <h2 class="text-white text-3xl font-bold uppercase">{{ data?.title }}</h2>
-                    <p class="text-white text-base font-normal" style="word-break: auto-phrase;">{{ data?.description }}</p>
+                    <h2 class="text-white text-3xl font-bold uppercase whitespace-nowrap">{{ data?.title }}</h2>
+                    <p class="text-white lg:text-base text-sm font-normal" style="word-break: auto-phrase;">{{ data?.description }}</p>
                 </div>
                 <a :href="`tel:${store.phone}`">
                     <button-pr class="w-fit min-w-[170px]" :hasIcon="false" size="default" type="primary" :label="data?.coming_soon ? 'In arrivo' : 'Prenota ora'" :disabled="data?.coming_soon" />
